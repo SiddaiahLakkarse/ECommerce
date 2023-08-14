08c724a70b09686613ebe43e08140bc04cdca20e
SidduLakkarse <SidduLakkarse>
2023-08-14 18:01:30 +0530

    Creating a consistent error response from the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,9 +10,7 @@ using AutoMapper;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[Controller]")]
-  public class ProductsController : ControllerBase
+  public class ProductsController : BaseApiController
   {
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
     private readonly IGenericRepository<ProductType> _productTypeRepo;
