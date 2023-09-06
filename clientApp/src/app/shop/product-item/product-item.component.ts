4e363312fdbb865844134849e78d2e51571731bf
SidduLakkarse <SidduLakkarse>
2023-09-06 12:39:03 +0530

    Passing down data to child components

--- clientApp/src/app/shop/product-item/product-item.component.ts
+++ clientApp/src/app/shop/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-item',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ProductItemComponent {
 
+  @Input() product?: Product;
+  constructor() { }
 }
